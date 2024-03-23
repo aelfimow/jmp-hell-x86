a3b7ecd5c30de41499fb72ff11366bf812ae97b8
Alexander Elfimow <my.free.mail@gmx.de>
2024-03-23 21:30:26 +0100

    jmp_hell: added file

---
+++ src/jmp_func.h
@@ -0,0 +1,48 @@
+#ifdef JMP_FUNC_H
+#error Already included
+#else
+
+#define JMP_FUNC_H      1
+
+extern "C"
+{
+    uint64_t jmp_hell_10();
+    uint64_t jmp_hell_20();
+    uint64_t jmp_hell_30();
+    uint64_t jmp_hell_40();
+    uint64_t jmp_hell_50();
+    uint64_t jmp_hell_60();
+    uint64_t jmp_hell_70();
+    uint64_t jmp_hell_80();
+    uint64_t jmp_hell_90();
+    uint64_t jmp_hell_100();
+    uint64_t jmp_hell_200();
+    uint64_t jmp_hell_300();
+    uint64_t jmp_hell_400();
+    uint64_t jmp_hell_500();
+    uint64_t jmp_hell_600();
+    uint64_t jmp_hell_700();
+    uint64_t jmp_hell_800();
+    uint64_t jmp_hell_900();
+    uint64_t jmp_hell_1000();
+    uint64_t jmp_hell_2000();
+    uint64_t jmp_hell_3000();
+    uint64_t jmp_hell_4000();
+    uint64_t jmp_hell_5000();
+    uint64_t jmp_hell_6000();
+    uint64_t jmp_hell_7000();
+    uint64_t jmp_hell_8000();
+    uint64_t jmp_hell_9000();
+    uint64_t jmp_hell_10000();
+    uint64_t jmp_hell_20000();
+    uint64_t jmp_hell_30000();
+    uint64_t jmp_hell_40000();
+    uint64_t jmp_hell_50000();
+    uint64_t jmp_hell_60000();
+    uint64_t jmp_hell_70000();
+    uint64_t jmp_hell_80000();
+    uint64_t jmp_hell_90000();
+    uint64_t jmp_hell_100000();
+}
+
+#endif
