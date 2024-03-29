0d3143a9c96e77788f19feb0ab738f3302201b7b
Alexander Elfimow <my.free.mail@gmx.de>
2024-03-29 20:59:59 +0100

    main: use new struct

--- src/main.cpp
+++ src/main.cpp
@@ -17,149 +17,96 @@ struct jmp_hell_func
 int main(int, char *[])
 try
 {
-    std::vector<std::function<uint64_t ()>> const all_functions
+    std::vector<jmp_hell_func> all_functions
     {
-        jmp_hell_10,
-        jmp_hell_20,
-        jmp_hell_30,
-        jmp_hell_40,
-        jmp_hell_50,
-        jmp_hell_60,
-        jmp_hell_70,
-        jmp_hell_80,
-        jmp_hell_90,
-        jmp_hell_100,
-        jmp_hell_200,
-        jmp_hell_300,
-        jmp_hell_400,
-        jmp_hell_500,
-        jmp_hell_600,
-        jmp_hell_700,
-        jmp_hell_800,
-        jmp_hell_900,
-        jmp_hell_1000,
-        jmp_hell_2000,
-        jmp_hell_3000,
-        jmp_hell_4000,
-        jmp_hell_5000,
-        jmp_hell_6000,
-        jmp_hell_7000,
-        jmp_hell_8000,
-        jmp_hell_9000,
-        jmp_hell_10000,
-        jmp_hell_20000,
-        jmp_hell_30000,
-        jmp_hell_40000,
-        jmp_hell_50000,
-        jmp_hell_60000,
-        jmp_hell_70000,
-        jmp_hell_80000,
-        jmp_hell_90000,
-        jmp_hell_100000
+        { jmp_hell_10, 10U, 0U, 0U },
+        { jmp_hell_20, 20U, 0U, 0U },
+        { jmp_hell_30, 30U, 0U, 0U },
+        { jmp_hell_40, 40U, 0U, 0U },
+        { jmp_hell_50, 50U, 0U, 0U },
+        { jmp_hell_60, 60U, 0U, 0U },
+        { jmp_hell_70, 70U, 0U, 0U },
+        { jmp_hell_80, 80U, 0U, 0U },
+        { jmp_hell_90, 90U, 0U, 0U },
+        { jmp_hell_100, 100U, 0U, 0U },
+        { jmp_hell_200, 200U, 0U, 0U },
+        { jmp_hell_300, 300U, 0U, 0U },
+        { jmp_hell_400, 400U, 0U, 0U },
+        { jmp_hell_500, 500U, 0U, 0U },
+        { jmp_hell_600, 600U, 0U, 0U },
+        { jmp_hell_700, 700U, 0U, 0U },
+        { jmp_hell_800, 800U, 0U, 0U },
+        { jmp_hell_900, 900U, 0U, 0U },
+        { jmp_hell_1000, 1000U, 0U, 0U },
+        { jmp_hell_2000, 2000U, 0U, 0U },
+        { jmp_hell_3000, 3000U, 0U, 0U },
+        { jmp_hell_4000, 4000U, 0U, 0U },
+        { jmp_hell_5000, 5000U, 0U, 0U },
+        { jmp_hell_6000, 6000U, 0U, 0U },
+        { jmp_hell_7000, 7000U, 0U, 0U },
+        { jmp_hell_8000, 8000U, 0U, 0U },
+        { jmp_hell_9000, 9000U, 0U, 0U },
+        { jmp_hell_10000, 10000U, 0U, 0U },
+        { jmp_hell_20000, 20000U, 0U, 0U },
+        { jmp_hell_30000, 30000U, 0U, 0U },
+        { jmp_hell_40000, 40000U, 0U, 0U },
+        { jmp_hell_50000, 50000U, 0U, 0U },
+        { jmp_hell_60000, 60000U, 0U, 0U },
+        { jmp_hell_70000, 70000U, 0U, 0U },
+        { jmp_hell_80000, 80000U, 0U, 0U },
+        { jmp_hell_90000, 90000U, 0U, 0U },
+        { jmp_hell_100000, 100000U, 0U, 0U }
     };
 
-    std::valarray<uint64_t> diff_min(all_functions.size());
-    std::valarray<uint64_t> diff_max(all_functions.size());
-
-    std::vector<size_t> const index_values
+    // First run to initialize min and max
     {
-        10U,
-        20U,
-        30U,
-        40U,
-        50U,
-        60U,
-        70U,
-        80U,
-        90U,
-        100U,
-        200U,
-        300U,
-        400U,
-        500U,
-        600U,
-        700U,
-        800U,
-        900U,
-        1000U,
-        2000U,
-        3000U,
-        4000U,
-        5000U,
-        6000U,
-        7000U,
-        8000U,
-        9000U,
-        10000U,
-        20000U,
-        30000U,
-        40000U,
-        50000U,
-        60000U,
-        70000U,
-        80000U,
-        90000U,
-        100000U
-    };
+        uint64_t T0 = jmp_hell_10();
 
-    std::vector<bool> const checks
-    {
-        (diff_min.size() == diff_max.size()),
-        (index_values.size() == diff_max.size()),
-        (index_values.size() == diff_min.size())
-    };
-
-    if (not std::all_of(checks.cbegin(), checks.cend(), [](bool flag) { return flag; }))
-    {
-        const std::string size_check { "Size check failed" };
-        throw std::invalid_argument(size_check);
-    }
-
-    // First run to initialize diff_min and diff_max
-    {
-        size_t cnt = 0U;
-
-        uint64_t T0 = jmp_hell_100000();
-
-        for (auto function: all_functions)
+        for (auto &function: all_functions)
         {
-            uint64_t const T1 = function();
+            uint64_t const T1 = function.func();
 
             uint64_t const diff = (T1 - T0);
 
-            diff_min[cnt] = diff;
-            diff_max[cnt] = diff;
-
-            ++cnt;
+            function.min = diff;
+            function.max = diff;
 
             T0 = T1;
         }
     }
 
-    for (size_t step = 0U; step < 100000U; ++step)
+    for (size_t step = 0U; step < 1000U; ++step)
     {
-        size_t cnt = 0U;
+        uint64_t T0 = jmp_hell_10();
 
-        uint64_t T0 = jmp_hell_100000();
-
-        for (auto function: all_functions)
+        for (auto &function: all_functions)
         {
-            uint64_t const T1 = function();
+            uint64_t const T1 = function.func();
 
             uint64_t const diff = (T1 - T0);
 
-            diff_min[cnt] = std::min(diff_min[cnt], diff);
-            diff_max[cnt] = std::max(diff_max[cnt], diff);
+            if (diff < function.min)
+            {
+                function.min = diff;
+            }
 
-            ++cnt;
+            if (diff > function.max)
+            {
+                function.max = diff;
+            }
 
             T0 = T1;
         }
     }
 
-    for (size_t i = 0U; i < diff_min.size(); ++i)
+
+    for (auto const &function: all_functions)
     {
-        std::cout << index_values[i] << ": " << diff_min[i] << " ... " << diff_max[i] << std::endl;
+        auto const &jmp_count = function.jmp_count;
+        auto const &min = function.min;
+        auto const &max = function.max;
+
+        std::cout << jmp_count << ": " << min << "; " << max << std::endl;
     }
 
     return EXIT_SUCCESS;
